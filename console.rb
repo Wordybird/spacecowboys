9eeae4ccfbf154078fbf09ba061f9f4a063f3053
Wordybird <ben.a.faulkner@btinternet.com>
2016-11-15 15:30:49 +0000

    Update function working as intended.

--- console.rb
+++ console.rb
@@ -9,16 +9,25 @@ space_cowboy1=SpaceCowboy.new({
   'bounty_value'=>50000,
   'danger_level'=>'high'
   })
+
 space_cowboy2=SpaceCowboy.new({
   'name'=>'Obiwan',
   'species'=>'Earthling',
   'bounty_value'=>50000,
   'danger_level'=>'high'
   })
+
 space_cowboy1.save()
 space_cowboy2.save()
-# space_cowboy1.delete()
+
+space_cowboy1.name="An SQL Database"
+space_cowboy1.update()
+
+space_cowboy1.delete()
+
 space_cowboys = SpaceCowboy.all
+
 binding.pry
+
 
 nil
